6e3d4fa34029d5412ad6ded1a31dea637c094762
naracodes <naralee0124@gmail.com>
2020-10-22 02:41:14 -0400

    add closeable instructions

--- public/client.js
+++ public/client.js
@@ -473,6 +473,13 @@ var Botkit = {
     }
 };
 
+window.onload = function(){
+    document.getElementById('close').onclick = function(){
+        this.parentNode.parentNode.parentNode
+        .removeChild(this.parentNode.parentNode);
+        return false;
+    };
+};
 
 (function () {
     // your page initialization code here
